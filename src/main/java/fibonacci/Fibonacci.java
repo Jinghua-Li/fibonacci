101f6695507bb5690a1bbf22cfa9cb09fe613c43
Jinghua Li <jinghuali@melco-resorts.com>
2022-02-24 17:25:50 +0800

    Refactor: use static method repleace constructor method

--- src/main/java/fibonacci/Fibonacci.java
+++ src/main/java/fibonacci/Fibonacci.java
@@ -1,18 +1,13 @@
 package fibonacci;
 
 public class Fibonacci {
-    private final int index;
-    public Fibonacci(int index) {
-        this.index = index;
-    }
-
-    public int getFibonacci() {
+    public static int getFibonacci(int index) {
         if (index == 0) {
             return 0;
         }
         if (index <= 2) {
             return 1;
         }
-        return new Fibonacci(index - 1).getFibonacci() + new Fibonacci(index - 2).getFibonacci();
+        return getFibonacci(index - 1) + getFibonacci(index - 2);
     }
 }
